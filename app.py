c5724990cbe16c0b57080a946fc2579cba76378a
sabaLev <lev.nikitin@gmail.com>
2026-02-07 18:33:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,41 @@
 import streamlit as st
-import pandas as pd
 
-st.title("Редактируемая таблица 2x2")
+st.title("Таблица с кнопками рядом")
 
-# Создаем DataFrame
-df = pd.DataFrame(
-    [[10, 20], 
-     [30, 40]],
-    columns=['Колонка A', 'Колонка B']
-)
+# Данные
+data = [[10, 20], [30, 40]]
 
-# Редактируемая таблица
-edited_df = st.data_editor(df)
-
-st.write("Измененные данные:")
-st.write(edited_df)
+# Отображение
+for i in range(2):
+    cols = st.columns([3, 1, 3, 1])  # значение1, кнопки1, значение2, кнопки2
+    
+    with cols[0]:
+        st.write(f"**{data[i][0]}**")
+    
+    with cols[1]:
+        col1, col2 = st.columns(2)
+        with col1:
+            if st.button("➖", key=f"a_dec_{i}"):
+                data[i][0] -= 1
+                st.rerun()
+        with col2:
+            if st.button("➕", key=f"a_inc_{i}"):
+                data[i][0] += 1
+                st.rerun()
+    
+    with cols[2]:
+        st.write(f"**{data[i][1]}**")
+    
+    with cols[3]:
+        col3, col4 = st.columns(2)
+        with col3:
+            if st.button("➖", key=f"b_dec_{i}"):
+                data[i][1] -= 1
+                st.rerun()
+        with col4:
+            if st.button("➕", key=f"b_inc_{i}"):
+                data[i][1] += 1
+                st.rerun()
+    
+    if i == 0:
+        st.divider()
