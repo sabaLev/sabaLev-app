9f4d905e0f3609201b02fd4e7d11bc6cb88f1f65
sabaLev <lev.nikitin@gmail.com>
2026-02-07 16:55:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,26 @@
 import streamlit as st
 
-# Минимальные стили
-st.markdown(
-    """
-    <style>
-    div.row-widget.stRadio > div {
-        flex-direction: row;
-        align-items: center;
-    }
-    div.row-widget.stRadio > div[role="radiogroup"] > label {
-        margin-right: 20px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+st.title("Форма регистрации")
 
-col1, col2 = st.columns(2)
+# Используем beta_columns если доступно
+try:
+    col1, col2 = st.columns(2, vertical_alignment="top")
+except:
+    col1, col2 = st.columns(2)
 
 with col1:
-    # Группируем элементы в контейнер
-    with st.container():
-        st.write("**Левая панель**")
-        st.checkbox("Согласен с условиями")
-        st.text_input("Логин", key="login")
-        st.radio("Выберите:", ["Да", "Нет"], horizontal=True)
+    st.subheader("Персональные данные")
+    st.text_input("Имя", key="first_name")
+    st.text_input("Фамилия", key="last_name")
+    st.date_input("Дата рождения")
+    st.radio("Пол", ["Мужской", "Женский"], horizontal=True)
 
 with col2:
-    with st.container():
-        st.write("**Правая панель**")
-        st.checkbox("Получать уведомления")
-        st.text_input("Пароль", type="password", key="pass")
-        st.select_slider("Уровень", options=["Низкий", "Средний", "Высокий"])
+    st.subheader("Контактная информация")
+    st.text_input("Email", type="default")
+    st.text_input("Телефон", placeholder="+7 XXX XXX XX XX")
+    st.text_area("Адрес", height=100)
+    st.checkbox("Я согласен на обработку данных")
+
+# Кнопка вне колонок
+st.button("Зарегистрироваться", type="primary")
