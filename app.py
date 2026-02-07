5b0ba89d27ec97e0ad1b8f5d5c88378e6ff79a5c
sabaLev <lev.nikitin@gmail.com>
2026-02-07 18:35:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,15 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Таблица с кнопками рядом")
+df = pd.DataFrame({
+    'Цена': [100, 200],
+    'Кол-во': [5, 3]
+})
 
-# Данные
-data = [[10, 20], [30, 40]]
-
-# Отображение
-for i in range(2):
-    cols = st.columns([3, 1, 3, 1])  # значение1, кнопки1, значение2, кнопки2
-    
-    with cols[0]:
-        st.write(f"**{data[i][0]}**")
-    
-    with cols[1]:
-        col1, col2 = st.columns(2)
-        with col1:
-            if st.button("➖", key=f"a_dec_{i}"):
-                data[i][0] -= 1
-                st.rerun()
-        with col2:
-            if st.button("➕", key=f"a_inc_{i}"):
-                data[i][0] += 1
-                st.rerun()
-    
-    with cols[2]:
-        st.write(f"**{data[i][1]}**")
-    
-    with cols[3]:
-        col3, col4 = st.columns(2)
-        with col3:
-            if st.button("➖", key=f"b_dec_{i}"):
-                data[i][1] -= 1
-                st.rerun()
-        with col4:
-            if st.button("➕", key=f"b_inc_{i}"):
-                data[i][1] += 1
-                st.rerun()
-    
-    if i == 0:
-        st.divider()
+edited = st.data_editor(
+    df,
+    column_config={
+        'Цена': st.column_config.NumberColumn(step=10),
+        'Кол-во': st.column_config.NumberColumn(step=1)
+    }
+)
