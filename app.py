a02e5829c803ddb9a3373e290852e9831e37308b
sabaLev <lev.nikitin@gmail.com>
2026-02-07 18:31:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,17 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Таблица 2x2")
+st.title("Редактируемая таблица 2x2")
 
-# Данные
-data = [[0, 0], [0, 0]]
+# Создаем DataFrame
+df = pd.DataFrame(
+    [[10, 20], 
+     [30, 40]],
+    columns=['Колонка A', 'Колонка B']
+)
 
-# Отображение
-for i in range(2):
-    cols = st.columns(2)
-    for j in range(2):
-        with cols[j]:
-            st.write(f"**Ячейка [{i},{j}]**")
-            col1, col2, col3 = st.columns(3)
-            with col1:
-                if st.button("➖", key=f"dec_{i}_{j}"):
-                    data[i][j] -= 1
-                    st.rerun()
-            with col2:
-                st.write(f"**{data[i][j]}**")
-            with col3:
-                if st.button("➕", key=f"inc_{i}_{j}"):
-                    data[i][j] += 1
-                    st.rerun()
-    if i == 0:
-        st.divider()
+# Редактируемая таблица
+edited_df = st.data_editor(df)
+
+st.write("Измененные данные:")
+st.write(edited_df)
