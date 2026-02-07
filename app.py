29e8c2f56a3797165ffac721fa39b86a0f6c3f78
sabaLev <lev.nikitin@gmail.com>
2026-02-07 16:53:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,25 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-# CSS фикс
-st.markdown("""
-<style>
-@media (max-width: 640px) {
-    div[data-testid="column"] {
-        min-width: calc(50% - 1rem) !important;
-    }
-}
-</style>
-""", unsafe_allow_html=True)
+# Создаем основной контейнер
+main_container = st.container()
 
-# Создаем две колонки
-left_col, right_col = st.columns(2)
-
-with left_col:
-    st.checkbox("Опция A")
-    st.text_input("Имя", key="name_left")
+with main_container:
+    # Создаем две колонки
+    left_col, right_col = st.columns(2)
     
-with right_col:
-    st.checkbox("Опция B")
-    st.text_input("Имя", key="name_right")
+    # Левая колонка в своем контейнере
+    with left_col:
+        col1_container = st.container()
+        with col1_container:
+            st.checkbox("Чекбокс 1", key="cb1")
+            st.text_input("Введите текст 1", key="ti1")
+            st.slider("Слайдер 1", 0, 100, 50, key="s1")
+    
+    # Правая колонка в своем контейнере
+    with right_col:
+        col2_container = st.container()
+        with col2_container:
+            st.checkbox("Чекбокс 2", key="cb2")
+            st.text_input("Введите текст 2", key="ti2")
+            st.slider("Слайдер 2", 0, 100, 30, key="s2")
