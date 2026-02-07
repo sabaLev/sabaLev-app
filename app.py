6e391a0ec220b1a2224405c7fb750c38c99904cf
sabaLev <lev.nikitin@gmail.com>
2026-02-07 17:04:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,37 @@
+# Минимальный CSS фикс (добавьте в начало)
+st.markdown("""
+<style>
+@media (max-width: 640px) {
+    div[data-testid="column"] {
+        min-width: unset !important;
+    }
+}
+</style>
+""", unsafe_allow_html=True)
+
 import streamlit as st
 
-st.title("Форма регистрации")
+st.set_page_config(layout="wide")
 
-# Используем beta_columns если доступно
-try:
-    col1, col2 = st.columns(2, vertical_alignment="top")
-except:
-    col1, col2 = st.columns(2)
+# Создаем основной контейнер
+main_container = st.container()
 
-with col1:
-    st.subheader("Персональные данные")
-    st.text_input("Имя", key="first_name")
-    st.text_input("Фамилия", key="last_name")
-    st.date_input("Дата рождения")
-    st.radio("Пол", ["Мужской", "Женский"], horizontal=True)
-
-with col2:
-    st.subheader("Контактная информация")
-    st.text_input("Email", type="default")
-    st.text_input("Телефон", placeholder="+7 XXX XXX XX XX")
-    st.text_area("Адрес", height=100)
-    st.checkbox("Я согласен на обработку данных")
-
-# Кнопка вне колонок
-st.button("Зарегистрироваться", type="primary")
+with main_container:
+    # Создаем две колонки
+    left_col, right_col = st.columns(2)
+    
+    # Левая колонка в своем контейнере
+    with left_col:
+        col1_container = st.container()
+        with col1_container:
+            st.checkbox("Чекбокс 1", key="cb1")
+            st.text_input("Введите текст 1", key="ti1")
+            st.slider("Слайдер 1", 0, 100, 50, key="s1")
+    
+    # Правая колонка в своем контейнере
+    with right_col:
+        col2_container = st.container()
+        with col2_container:
+            st.checkbox("Чекбокс 2", key="cb2")
+            st.text_input("Введите текст 2", key="ti2")
+            st.slider("Слайдер 2", 0, 100, 30, key="s2")
